95fa7fc78862f491b9d3c19110bd6207bd606b05
Josue Reyes Rivera <li_josuereyes@unca.edu.mx>
2023-04-24 01:56:04 -0500

    Consultas corregidas

--- src/controllers/registers.controller.js
+++ src/controllers/registers.controller.js
@@ -1,16 +1,20 @@
+import moment from "moment/moment.js";
+import session from "express-session";
 import { pool } from "../db.js"
 
 // CRUD
 
 export const createRegister = async (req, res) => {
     try {
-        const { date, start_time, leave_time, id_computer, id_user, id_center } = req.body
+        const date = moment().format("DD/MM/YYYY")
+        const start_time = moment().format('h:mm:ss A')
+        const { id_computer, id_user, id_center } = req.body
         
-        if (!date || !start_time || !leave_time || !id_computer || !id_user || !id_center){
+        if (!date || !start_time || !id_computer || !id_user || !id_center){
             return res.status(500).json({ messaje: 'DATA IS NEEDED' })
         }
     
-        await pool.query('INSERT INTO registers(date, start_time, leave_time, id_computer, id_user, id_center) VALUES(?, ?, ?, ?, ?, ?)',[date, start_time, leave_time, id_computer, id_user, id_center])
+        await pool.query('INSERT INTO registers(date, start_time, id_computer, id_user, id_center) VALUES(?, ?, ?, ?, ?)',[date, start_time, id_computer, id_user, id_center])
     
         res.sendStatus(200)
 
@@ -35,7 +39,8 @@ export const readRegister = async (req, res) => {
 
 export const updateRegister = async (req, res) => {
     try {
-        const { date, start_time, leave_time, id_computer, id_user, id_center } = req.body
+        const { date, start_time, id_computer, id_user, id_center } = req.body
+        const leave_time = moment().format('h:mm:ss A')
         const id = req.params.id
 
         const [ result ] = await pool.query('UPDATE registers SET date = IFNULL(?, date), start_time = IFNULL(?, start_time), leave_time = IFNULL(?, leave_time),id_computer = IFNULL(?, id_computer),id_user = IFNULL(?, id_user),id_center = IFNULL(?, id_center) WHERE id = ?',[date, start_time, leave_time, id_computer, id_user, id_center, id])
@@ -67,18 +72,68 @@ export const deleteRegister = async (req, res) => {
 }
 
 export const readRegisters = async (req, res) => {
-    try {
+    try {            
         let page = parseInt(req.query.page)
-        let pageSize = 5
+        let pageSize = 10
 
         const startIndex = (page - 1) * pageSize;
         const endIndex = startIndex + pageSize;
-        
-        const [ rows ] = await pool.query('SELECT *FROM registers LIMIT ?, ?',[startIndex, pageSize])
-        
-        let [ totalCount ] = await pool.query('SELECT COUNT(*) as total FROM registers')
 
-        totalCount = totalCount[0].total
+        const sqlAC = `
+                SELECT DISTINCT r.id, r.date, r.start_time, r.leave_time, cmp.model as computer, u.username, c.name as center
+                FROM registers r
+                JOIN computers cmp ON r.id_computer = cmp.id
+                JOIN centers c ON r.id_center = c.id
+                JOIN users u ON r.id_user = u.id
+                JOIN users_centers uc ON r.id_center = uc.id_center
+                WHERE u.id = ${session.userId}
+                OR (uc.rol = 'admin' AND uc.id_user = ${session.userId} )
+                ORDER BY id DESC LIMIT ${startIndex}, ${pageSize}
+        `;
+
+        const sqlAC2 = `
+                SELECT COUNT(DISTINCT r.id) AS totalCount
+                FROM registers r
+                JOIN computers cmp ON r.id_computer = cmp.id
+                JOIN centers c ON r.id_center = c.id
+                JOIN users u ON r.id_user = u.id
+                JOIN users_centers uc ON r.id_center = uc.id_center
+                WHERE u.id = ${session.userId}
+                OR (uc.rol = 'admin' AND uc.id_user = ${session.userId} )
+        `;
+
+        const sqlR = `
+                SELECT r.id, r.date, r.start_time, r.leave_time, cmp.model as computer, u.username, c.name as center
+                FROM registers r
+                JOIN computers cmp ON r.id_computer = cmp.id
+                JOIN centers c ON r.id_center = c.id
+                JOIN users u ON r.id_user = u.id
+                ORDER BY id DESC LIMIT ${startIndex}, ${pageSize}
+        `;
+
+        const sqlR2 = `
+                SELECT COUNT(r.id) AS totalCount
+                FROM registers r
+                JOIN computers cmp ON r.id_computer = cmp.id
+                JOIN centers c ON r.id_center = c.id
+                JOIN users u ON r.id_user = u.id
+        `;
+
+        let sql = sqlAC
+        let sql2 = sqlAC2
+
+        if(session.root === true){
+            sql = sqlR
+            sql2 = sqlR2
+        }
+
+        
+        const [ rows ] = await pool.query(sql)
+
+        
+        let [ totalCount ] = await pool.query(sql2)
+
+        totalCount = totalCount[0].totalCount
 
         let prevPage = null;
         if (startIndex > 0) {
